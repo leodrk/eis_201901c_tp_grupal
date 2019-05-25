8bf9c22a9d48000887984debca5daca05799e1f6
Eric Arnez <ericarnez@gmail.com>
2019-05-25 19:28:45 -0300

    Agregado feature 1 Scenario 3 "Si la celda está ocupada por un enemigo, entonces se muere." minor fixes

--- src/main/java/gradle/cucumber/cuerposEnCeldas/Pared.java
+++ src/main/java/gradle/cucumber/cuerposEnCeldas/Pared.java
@@ -1,7 +1,13 @@
 package gradle.cucumber.cuerposEnCeldas;
 
+import gradle.cucumber.Celda;
+
 public abstract class Pared extends Cuerpo {
-    public boolean sePuedeAtravezar(){
+    public Pared(Celda celdaActual) {
+        super(celdaActual);
+    }
+
+    public boolean sePuedeAtravezar(Bomberman bomberman){
         return false;
     }
 }
