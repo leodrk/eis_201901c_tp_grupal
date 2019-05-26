5d1d7aa08c34fa6f6eb681dceadd48e0795eef66
leodrk <martos.marcelo@gmail.com>
2019-05-26 01:32:16 -0300

    agregado caso de uso para bomberman puede dejar bombas

--- src/test/java/gradle/cucumber/BombermanDejaBombas.java
+++ src/test/java/gradle/cucumber/BombermanDejaBombas.java
@@ -3,8 +3,11 @@ package gradle.cucumber;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import gradle.cucumber.cuerposEnCeldas.Acero;
 import gradle.cucumber.cuerposEnCeldas.Bomberman;
+import gradle.cucumber.cuerposEnCeldas.Enemigo;
 import gradle.cucumber.cuerposEnCeldas.Melamina;
+import gradle.cucumber.cuerposEnCeldas.ProtoMaxUnits;
 
 import static junit.framework.TestCase.assertTrue;
 
@@ -13,7 +16,6 @@ public class BombermanDejaBombas {
     private Celda celdaNueva = new Celda();
     private Melamina melamina;
     private Bomberman bomberman;
-    private Bomberman bomberman2;
     private Celda celdaCentral = new Celda();
     private Celda celdaAlEste3 = new Celda();
     private Celda celdaAlOeste3 = new Celda();
@@ -48,7 +50,7 @@ public class BombermanDejaBombas {
     
     @Given("^una Celda con un Bomberman rodeado de paredes de melamina en un radio de 3 celdas$")
     public void una_Celda_con_un_Bomberman_rodeado_de_paredes_de_melamina_en_un_radio_de_3_celdas() {
-    	bomberman2 = new Bomberman(celdaCentral);
+    	bomberman = new Bomberman(celdaCentral);
     	
     	celdaCentral.setCeldaAlEste(new Celda());
     	celdaCentral.getCeldaAlEste().setCeldaAlEste(new Celda());
@@ -73,7 +75,7 @@ public class BombermanDejaBombas {
     
     @When("^bomberman deja otra bomba de \"([^\"]*)\"$")
     public void bomberman_deja_otra_bomba_de(String tiempo) {
-    	bomberman2.dejarBomba(Integer.parseInt(tiempo));
+    	bomberman.dejarBomba(Integer.parseInt(tiempo));
     }
 
     @Then("^la bomba explota y rompe todas las paredes$")
@@ -88,4 +90,35 @@ public class BombermanDejaBombas {
     	assertTrue(celdaAlSur3.getCuerpoActual() == null);
     	assertTrue(celdaAlNorte3.getCuerpoActual() == null);
     }
+    
+    
+    @Given ("^una Celda con un bomberman y un enemigo en la celda al \"([^\"]*)\"$")
+    public void una_celda_con_un_bomberman_y_un_enemigo_en_la_celda_al(String dir) {
+    	celdaCentral = new Celda();
+    	bomberman = new Bomberman(celdaCentral);
+    	celdaNueva = new Celda();
+    	celdaCentral.setCeldaAl(dir, celdaNueva);
+    	Enemigo enemy = new Enemigo(celdaNueva);
+    }
+
+    @Then("^la bomba explota y mata al enemigo$")
+	public void la_bomba_explota_y_mata_al_enemigo(){
+    	
+    	celdaCentral.tick();
+    	assertTrue (celdaNueva.getCuerpoActual() == null);
+    }
+    
+    @Given ("^una Celda con un bomberman y una pared de acero en la celda al \"([^\"]*)\"$")
+    public void una_celda_con_un_bomberman_y_una_pared_de_acero_en_la_celda_al(String dir){
+    	celdaCentral = new Celda();
+    	bomberman = new Bomberman(celdaCentral);
+    	celdaNueva = new Celda();
+    	celdaCentral.setCeldaAl(dir, celdaNueva);
+    	Acero paredAcero = new Acero(celdaNueva);
+    }
+ 	@Then ("^la bomba explota pero no rompe la pared$")
+ 	public void la_bomba_explora_pero_no_rompe_la_pared(){
+ 		celdaCentral.tick();
+    	assertTrue (celdaNueva.getCuerpoActual() != null);
+ 	}
 }
